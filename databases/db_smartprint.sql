22f3b8988efac28896b9ed8664d8bfcc30a02e3f
VSNDEV\faisal.alfareza <Password1!>
2018-11-03 11:38:47 +0700

    .

--- databases/db_smartprint.sql
+++ databases/db_smartprint.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: 01 Nov 2018 pada 07.40
+-- Generation Time: 03 Nov 2018 pada 05.38
 -- Versi Server: 10.1.13-MariaDB
 -- PHP Version: 7.0.8
 
@@ -52,8 +52,16 @@ INSERT INTO `ms_article` (`ArtclId`, `ArtclTitle`, `ArtclContent`, `ArtclType`, 
 
 CREATE TABLE `ms_batch_merchant` (
   `MerchantId` int(20) NOT NULL,
-  `MerchantQueueNumber` bigint(100) NOT NULL
+  `MerchantQueueNumber` bigint(100) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `ms_batch_merchant`
+--
+
+INSERT INTO `ms_batch_merchant` (`MerchantId`, `MerchantQueueNumber`) VALUES
+(2, 2),
+(1, 4);
 
 -- --------------------------------------------------------
 
@@ -94,15 +102,24 @@ CREATE TABLE `ms_merchant` (
   `MerchantId` int(20) NOT NULL,
   `MerchantCode` varchar(6) NOT NULL,
   `MerchantName` varchar(50) NOT NULL,
-  `MerchantAddress` varchar(70) NOT NULL,
-  `MerchantTelp` varchar(13) NOT NULL,
-  `MerchantEmail` varchar(30) NOT NULL,
-  `EstablishedDate` varchar(30) NOT NULL,
-  `HowManyFeedback` bigint(100) NOT NULL,
-  `WorkTime` varchar(30) NOT NULL,
-  `LongLat` varchar(50) NOT NULL,
+  `MerchantAddress` text NOT NULL,
+  `MerchantDesc` text,
+  `MerchantTelp` text NOT NULL,
+  `MerchantEmail` text NOT NULL,
+  `EstablishedDate` varchar(30) DEFAULT NULL,
+  `HowManyFeedback` bigint(100) DEFAULT NULL,
+  `WorkTime` varchar(30) DEFAULT NULL,
+  `LongLat` varchar(50) DEFAULT NULL,
   `UserId` int(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `ms_merchant`
+--
+
+INSERT INTO `ms_merchant` (`MerchantId`, `MerchantCode`, `MerchantName`, `MerchantAddress`, `MerchantDesc`, `MerchantTelp`, `MerchantEmail`, `EstablishedDate`, `HowManyFeedback`, `WorkTime`, `LongLat`, `UserId`) VALUES
+(1, 'MSTRO', 'Maestro Percetakan', 'Jl. Raya Gubeng No.30, Gubeng, Surabaya City, East Java 60281', 'Maestro merupakan perusahaan outlet fotokopi, digital printing dan advertising terbesar di Malang dengan konsep pelayanan ONE STOP SERVICE. Semua dikerjakan di satu tempat mulai dari desain, produksi sampai produk jadi. Dengan didukung dengan tenaga- tenaga yang selalu berinovasi, profesional, berpengalaman dan berdedikasi tinggi serta teknologi terkini. Maestro siap memberikan layanan terbaik di bidangnya.', '(031) 5051888', 'info@maestrophotocopy.com', '2006', 5, '08.00-21.00', '12', 171),
+(2, 'MJST', 'Majesty Printing', 'Jl. Brigjend Slamet Riadi No.44, Oro-oro Dowo, Klojen, Kota Malang, Jawa Timur 65112', 'Majesty Printing telah berdiri sejak tahun 2005. Seiring dengan berjalannya waktu, Majesty Printing selalu berinovasi dengan menghadirkan produk-produk yang dibutuhkan oleh masyarakat. Mulai dari print banner indoor, outdoor, laser, T-shirt, hingga cetak offset dapat terlayani. Selain itu, pembuatan produk-produk seperti brosur, undangan, pin, mug, kalender, buku, hingga branding kendaraan juga dapat terlayani, sehingga Majesty Printing menjadi One Stop Printing, yaitu pelayanan di bidang printing di mana masyarakat bisa mendapatkan berbagai produk printing sekaligus di satu tempat.\r\n\r\nBerbekal pengalaman lebih dari sepuluh tahun, Majesty Printing berkomitmen untuk terus memberikan pelayanan yang terbaik di bidangnya, sembari tetap mempertahankan kualitas hasil cetakan yang dihasilkan. Sehingga harga yang ditetapkan dengan kualitas yang didapatkan selalu lebih murah.', '0341-369800,0818387744,0818387744', 'majesty_printing@yahoo.com,majesty.printing@gmail.com', '2005', 0, '08.00–12.00', 'qsq', 152);
 
 -- --------------------------------------------------------
 
@@ -266,15 +283,9 @@ CREATE TABLE `ms_user` (
 --
 
 INSERT INTO `ms_user` (`UserId`, `UserEmail`, `UserPass`, `UserStatus`) VALUES
-(95, 'faisal.alfareza@visionet.co.id', '$2a$08$PLjZ4KFu/2a7eB9Nwl3fnOY1H1Hfg4EY5A.tulgkN1WvIxYJhcl7q', '1'),
-(96, 'admin@neogeeks.co.id', '$2a$08$j08MdPXpYbrtQAfO2tWUmOHvmnCPqa9dwXUVi1Nwwz.aeREEX1O/e', '1'),
-(100, 'bamas.angkasa@visionet.co.id', '$2a$08$wWFjoUztAO.rFXua4ObeoO8LRFpuxNHegEu/qeIzRa3lVTrLsvxte', '1'),
-(152, 'alde.asprilla@visionet.co.id', '$2a$08$Ayv9R/N/3XZsHj20OODteeLOWeT6saPTe0gTyHTij1.NRRIEwb06S', '1'),
-(157, 'yayan.yayan98@gmail.com', '$2a$08$F73r6cIjH4QX2SjK/hX3rOFDPPYJuiT/ktemtT8HGgLeaZkppGa8e', '1'),
-(167, 'imas.setyawan@gmail.com', '$2a$08$2o1TWOgvRgaDrU/dOjtCU.PBsbzTyIPpXg5qxeuQbBUBSr1wsZ4tK', '1'),
-(168, 'sakinahasalalita@gmail.com', '$2a$08$eVdvCrgRTFEWgSjzFg87j.OFZxNNcoVy4vZAXBye2JYcUOkiiV3we', '1'),
-(169, 'saso@gmai', '$2a$08$gtUMg1aOJX4cw5tBdFL0TeC2AjZyjpGi/Ma4BRvh89wksitLsDdf6', '1'),
-(170, 'stiki@gmail.com', '$2a$08$a8lSVAD9/2nrMHBeCf65hO/i1kPzP/1VOL.dXYiEVUhSFumwplP6.', '1');
+(95, 'user@smartprint.com', '$2a$08$PLjZ4KFu/2a7eB9Nwl3fnOY1H1Hfg4EY5A.tulgkN1WvIxYJhcl7q', '1'),
+(172, 'admin@smartprint.com', '$2a$08$XhpviPzBRFm9WnoTAunnQOeo0aJPqqClerJ7XNYSYaLcwq2ijNLkq', '1'),
+(171, 'merchant@smartprint.com', '$2a$08$7Hibs89mDtmQfL4qtGgPBOG.AN9X8IHBFfzF1HmCgLt/PC0AsRA/6', '1');
 
 -- --------------------------------------------------------
 
@@ -293,15 +304,9 @@ CREATE TABLE `ms_user_role` (
 --
 
 INSERT INTO `ms_user_role` (`UserRoleId`, `UserId`, `RoleId`) VALUES
-(56, 95, 1),
-(57, 96, 1),
-(59, 100, 2),
-(106, 152, 2),
-(111, 157, 2),
-(118, 167, 1),
-(119, 168, 2),
-(120, 169, 2),
-(121, 170, 2);
+(56, 95, 2),
+(122, 171, 3),
+(123, 172, 1);
 
 -- --------------------------------------------------------
 
@@ -313,8 +318,24 @@ CREATE TABLE `tr_document` (
   `QueueNumber` varchar(100) NOT NULL,
   `DocumentId` int(20) NOT NULL,
   `MerchantId` int(20) NOT NULL,
-  `Status` enum('requested','inprogress','finished') NOT NULL
+  `UserId` int(20) NOT NULL,
+  `Status` enum('requested','inprogress','finished') NOT NULL,
+  `UploadedOn` varchar(70) DEFAULT NULL,
+  `ProcessedOn` varchar(20) DEFAULT NULL,
+  `FinishedOn` varchar(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `tr_document`
+--
+
+INSERT INTO `tr_document` (`QueueNumber`, `DocumentId`, `MerchantId`, `UserId`, `Status`, `UploadedOn`, `ProcessedOn`, `FinishedOn`) VALUES
+('MJST-95-02112018-094331-1', 65, 2, 95, 'inprogress', 'Friday, 02 Nov 2018 09:43:31', NULL, NULL),
+('MJST-95-02112018-094331-2', 66, 2, 95, 'requested', 'Friday, 02 Nov 2018 09:43:31', NULL, NULL),
+('MSTRO-95-02112018-095354-1', 67, 1, 95, 'requested', 'Friday, 02 Nov 2018 09:53:54', NULL, NULL),
+('MSTRO-95-02112018-100653-2', 68, 1, 95, 'inprogress', 'Friday, 02 Nov 2018 10:06:53', NULL, NULL),
+('MSTRO-95-02112018-100653-3', 69, 1, 95, 'inprogress', 'Friday, 02 Nov 2018 10:06:53', NULL, NULL),
+('MSTRO-95-02112018-164250-4', 70, 1, 95, 'requested', 'Friday, 02 Nov 2018 16:42:50', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -326,8 +347,23 @@ CREATE TABLE `tr_document_detail` (
   `DocumentId` int(20) NOT NULL,
   `DocumentName` varchar(50) NOT NULL,
   `DocumentType` varchar(15) NOT NULL,
-  `LinkFileUrl` text NOT NULL
+  `LinkFileUrl` text NOT NULL,
+  `FileName` text NOT NULL,
+  `Note` text,
+  `EstimationTime` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `tr_document_detail`
+--
+
+INSERT INTO `tr_document_detail` (`DocumentId`, `DocumentName`, `DocumentType`, `LinkFileUrl`, `FileName`, `Note`, `EstimationTime`) VALUES
+(65, 'Tugas Basis Data - Praktek Mysql', '.docx', 'E:/xampp/htdocs/project-codeigniter/smart-print/assets/images/uploads/upload-documents/MJST95-02112018-094331/TUGAS_PERTEMUAN_2_-_STORE_PROCEDURE.docx', 'TUGAS_PERTEMUAN_2_-_STORE_PROCEDURE.docx', 'Mohon untuk dicetak dengan rapi dan baik, karena akan dipergunakan sebagai dokumen penting untuk masa depan', ''),
+(66, 'Tugas Basis Data - Praktek Mysql', '.docx', 'E:/xampp/htdocs/project-codeigniter/smart-print/assets/images/uploads/upload-documents/MJST95-02112018-094331/TUGAS_PERTEMUAN_3_-_TRIGGER.docx', 'TUGAS_PERTEMUAN_3_-_TRIGGER.docx', 'Mohon untuk dicetak dengan rapi dan baik, karena akan dipergunakan sebagai dokumen penting untuk masa depan', ''),
+(67, 'Ticket Pergi - Visionet', '.PDF', 'E:/xampp/htdocs/project-codeigniter/smart-print/assets/images/uploads/upload-documents/MSTRO95-02112018-095354/QLCBXJ.PDF', 'QLCBXJ.PDF', 'Bewarna & Kertas Folio', '09 November 2018'),
+(68, 'Tugas Bahasa Indonesia', '.docx', 'E:/xampp/htdocs/project-codeigniter/smart-print/assets/images/uploads/upload-documents/MSTRO95-02112018-100652/TUGAS_2_-_161116039_Faisal_Alfareza_-_Koreksi_Artikel_-_Penggunaan_Smartphone_dan_Interaksi_Sosial_Pada_Remaja.docx', 'TUGAS_2_-_161116039_Faisal_Alfareza_-_Koreksi_Artikel_-_Penggunaan_Smartphone_dan_Interaksi_Sosial_Pada_Remaja.docx', 'pelayanan di bidang printing di mana masyarakat bisa mendapatkan berbagai produk printing sekaligus di satu tempat', '07 November 2018'),
+(69, 'Tugas Bahasa Indonesia', '.docx', 'E:/xampp/htdocs/project-codeigniter/smart-print/assets/images/uploads/upload-documents/MSTRO95-02112018-100652/TUGAS_4_-_161116039_Faisal_Alfareza_-_Teks_3_Bahasa.docx', 'TUGAS_4_-_161116039_Faisal_Alfareza_-_Teks_3_Bahasa.docx', 'pelayanan di bidang printing di mana masyarakat bisa mendapatkan berbagai produk printing sekaligus di satu tempat', '07 November 2018'),
+(70, 'UTS Pemrograman Perangkat Bergerak (PPB) - Subari', '.pdf', 'E:/xampp/htdocs/project-codeigniter/smart-print/assets/images/uploads/upload-documents/MSTRO95-02112018-164249/(Subari)_UTS_PPB_(Kelas-P).pdf', '(Subari)_UTS_PPB_(Kelas-P).pdf', 'Yang bagus dan rapi, nanti orangnya ngamuk ihihi ..', '10 November 2018');
 
 --
 -- Indexes for dumped tables
@@ -412,7 +448,8 @@ ALTER TABLE `ms_user_role`
 ALTER TABLE `tr_document`
   ADD PRIMARY KEY (`QueueNumber`),
   ADD KEY `DocumentId` (`DocumentId`),
-  ADD KEY `MerchantId` (`MerchantId`);
+  ADD KEY `MerchantId` (`MerchantId`),
+  ADD KEY `UserId` (`UserId`);
 
 --
 -- Indexes for table `tr_document_detail`
@@ -434,6 +471,11 @@ ALTER TABLE `ms_article`
 --
 ALTER TABLE `ms_category`
   MODIFY `CategoryId` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
+--
+-- AUTO_INCREMENT for table `ms_merchant`
+--
+ALTER TABLE `ms_merchant`
+  MODIFY `MerchantId` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `ms_news`
 --
@@ -463,12 +505,17 @@ ALTER TABLE `ms_role`
 -- AUTO_INCREMENT for table `ms_user`
 --
 ALTER TABLE `ms_user`
-  MODIFY `UserId` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=171;
+  MODIFY `UserId` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=173;
 --
 -- AUTO_INCREMENT for table `ms_user_role`
 --
 ALTER TABLE `ms_user_role`
-  MODIFY `UserRoleId` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=122;
+  MODIFY `UserRoleId` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=124;
+--
+-- AUTO_INCREMENT for table `tr_document_detail`
+--
+ALTER TABLE `tr_document_detail`
+  MODIFY `DocumentId` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=71;
 --
 -- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
 --
